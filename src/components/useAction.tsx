c99aa698dfadcd872be4eda6d7b20b968383eb8f
Carl Riis <carl.aagot.riis@gmail.com>
2020-06-24 20:02:09 +0200

    Action system improved for more cases

--- src/components/useAction.tsx
+++ src/components/useAction.tsx
@@ -11,10 +11,28 @@ type State = Action[];
 const ActionReducer = (state: State, option: Options) => {
   switch (option.type) {
     case "add":
-      if (typeof state[state.length - 1] === "number" || state.length === 0) {
-        return [...state, option.action];
+      // Making sure you can't use two operators after each other
+      if (
+        typeof state[state.length - 1] !== "number" &&
+        typeof option.action !== "number"
+      ) {
+        return state;
       }
-      return state;
+
+      if (
+        typeof state[state.length - 1] === "number" &&
+        typeof option.action === "number"
+      ) {
+        // Merging two digits. 1 + 1 = 11
+        return state.map((action, i) => {
+          if (i === state.length - 1) {
+            return parseInt(action.toString() + option.action.toString());
+          }
+          return action;
+        });
+      }
+
+      return [...state, option.action];
     case "clear":
       return [];
     default:
