8d7261d8f682785cf4a9396f24d689ed653d9f70
Carl Riis <carl.aagot.riis@gmail.com>
2020-06-28 16:59:58 +0200

    Updates

---
+++ precache-manifest.84bb0d543411ded7b464b957e4d1c884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d1431396dde24f6215c8b33e8c60c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abc58e80524242d8f978",
+    "url": "/static/css/main.481fc446.chunk.css"
+  },
+  {
+    "revision": "b6add1069da172bbdba6",
+    "url": "/static/js/2.7f02c42d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f02c42d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc58e80524242d8f978",
+    "url": "/static/js/main.db15c6ea.chunk.js"
+  },
+  {
+    "revision": "03029541fdfd5c241e04",
+    "url": "/static/js/runtime-main.cb72f7c3.js"
+  }
+]);
