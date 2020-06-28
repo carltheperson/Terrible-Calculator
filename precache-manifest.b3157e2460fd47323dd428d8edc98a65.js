8da764ad81b0e60310b466b64340ee617b3ee8a7
Carl Riis <carl.aagot.riis@gmail.com>
2020-06-28 22:01:10 +0200

    Updates

---
+++ precache-manifest.b3157e2460fd47323dd428d8edc98a65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeaf244b1447fb22e8063628c17ebedc",
+    "url": "https://carlriis.github.io/Terrible-Calculator/index.html"
+  },
+  {
+    "revision": "abc58e80524242d8f978",
+    "url": "https://carlriis.github.io/Terrible-Calculator/static/css/main.481fc446.chunk.css"
+  },
+  {
+    "revision": "b6add1069da172bbdba6",
+    "url": "https://carlriis.github.io/Terrible-Calculator/static/js/2.7f02c42d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://carlriis.github.io/Terrible-Calculator/static/js/2.7f02c42d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc58e80524242d8f978",
+    "url": "https://carlriis.github.io/Terrible-Calculator/static/js/main.db15c6ea.chunk.js"
+  },
+  {
+    "revision": "cb43c62f2de0e1b194d1",
+    "url": "https://carlriis.github.io/Terrible-Calculator/static/js/runtime-main.42b7e7d9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84bb0d543411ded7b464b957e4d1c884.js"
+  "https://carlriis.github.io/Terrible-Calculator/precache-manifest.b3157e2460fd47323dd428d8edc98a65.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://carlriis.github.io/Terrible-Calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
